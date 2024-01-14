a46d4fe2f00e28ac279e31b86085b235a7d26858
Souradip <dgsouro77@gmail.com>
2024-01-14 14:24:38 +0530

    Added websockets

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const {Server}=require("socket.io");
+const server=http.createServer(app);
+
+const io= new Server(server);
+
+io.on("connection", (socket)=>{
+    socket.on('user-message', (message)=>{
+
+       io.emit("message", message);
+    });
+});
+
+app.use(express.static(path.resolve("./public" )));
+
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html");
+})
+server.listen(8000, () => {
+    console.log(`Listening on 8000`)
+});
